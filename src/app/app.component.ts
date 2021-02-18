176ae0a6f5a19d2385cd240b6dd9255728e2e29b
Siddhartha Saxena <siddharthasaxena.1998@gmail.com>
2021-02-18 18:22:00 +0530

    Basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import {Todo} from './todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'youtubeAng';
+  title = 'Demo Project';
+  
+  todoValue:String="";
+  list:Todo[]=[];
+
+  ngOnInit(){
+    this.list=[];
+    this.todoValue="";
+  }
+  addItem() {
+    if(this.todoValue!==""){
+      const newItem:Todo={
+        id:Date.now(),
+        value:this.todoValue,
+        isDone:false
+      }
+      this.list.push(newItem);
+    }
+    this.todoValue="";
+  }
+  deleteItem(id:Number){
+    this.list=this.list.filter(item=>item.id!==id);
+  }
 }
